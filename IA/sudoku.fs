fd65f65d7a8465c3c354d5a12f50710182913209
KIIIIIIIIIIIII <129471648+KIIIIIIIIIIIII@users.noreply.github.com>
2023-04-05 23:45:29 -0600

    Update sudoku.fs

--- IA/sudoku.fs
+++ IA/sudoku.fs
@@ -11,6 +11,7 @@ module sudoku =
         | Siete
         | Ocho 
         | Nueve
+    
     type estado = list<list<int>*list<int>*list<int>*list<int>*list<int>*list<int>*list<int>*list<int>*list<int>>
 
     let inicio =   [[0;0;0 ;0;0;0; 0;0;0],
@@ -37,74 +38,40 @@ module sudoku =
                     [6;4;5 ;8;9;7; 2;3;1],
                     [9;7;8 ;2;3;1; 5;6;4]]
 
-
-    let sucesor i accion (estado : estado) =
-        let swap i j =
-            estado
-            |> List.mapi (fun indx x ->
-                    if indx = i then
-                        List.item j estado
-                    elif indx = j then
-                        List.item i estado
-                    else 
-                        x
-                )
-        match accion with
-        | Uno -> if i % 3 <> 0
-                    then Some (accion, swap i (i-1))
-                    else None
-        | Dos -> if i % 3 <> 2
-                    then Some (accion, swap i (i+1))
-                    else None
-        | Tres -> if i > 2
-                    then Some (accion, swap i (i-3))
-                    else None
-        | Cuatro-> if i < 6
-                    then Some (accion, swap i (i+3))
-                    else None
-
-(*
-
-
     let meta estado =
-        List.map2 (fun x y -> (x,y)) goal estado
-        |> List.forall (fun (x,y) -> x = y)
+        List.map2(fun x y ->(x,y))
+            goal estado
+        |> List.forall(fun(x,y)->x=y)
 
-    let cero estado =
-        List.findIndex (fun x -> x = 0) estado
+    let succesores (estado:estado)=
+        let indicesVacios = //comprueba si se encuentran valores 0 en alguna posicion de la lista
+            [ for i in 0..8 do
+                for j in 0..8 do
+                    if estado.[i].[j] = 0 then yield (i,j) ] //si encuentra un 0 regresa la pocicion donde se enontro el valor
 
-    let sucesor i accion (estado : estado) =
-        let swap i j =
-            estado
-            |> List.mapi (fun indx x ->
-                    if indx = i then
-                        List.item j estado
-                    elif indx = j then
-                        List.item i estado
-                    else 
-                        x
-                )
-        match accion with
-        | Left -> if i % 3 <> 0
-                    then Some (accion, swap i (i-1))
-                    else None
-        | Right -> if i % 3 <> 2
-                    then Some (accion, swap i (i+1))
-                    else None
-        | Up -> if i > 2
-                    then Some (accion, swap i (i-3))
-                    else None
-        | Down -> if i < 6
-                    then Some (accion, swap i (i+3))
-                    else None
-    let sucesores estado =
-        let indice = cero estado
-        [
-            sucesor indice Left estado
-            sucesor indice Right estado
-            sucesor indice Up estado
-            sucesor indice Down estado
-        ]   |> List.choose id
+        let movimientosValidos (i,j)=//comprueba los valores validos para la posicion donde se encontraron 0 para ello se lleva la posicion de donde se encontro el 0
+            let fila = estado.[i]//a fila le asigna la fila de la posicion del 0
+            let columna = [ for k in 0..8 -> estado.[k].[j] ]
+            let subcuadricula = //asigna la cuadricula donde se encontro un 0
+                [ for k in [0..2] do
+                    for l in [0..2] do
+                        yield estado.[i/3*3+k].[j/3*3+l] ]//regresa la cuadricula
+        
+            [1..9]//secuencia de posibles valores
+                |> Seq.filter (fun num -> not (num |> List.contains fila))//revisa si se pueden asignar valores a la fila encontrada anteriormente dentro de una secuencia
+                |> Seq.filter (fun num -> not (num |> List.contains columna))//asoigna valores a la columna 
+                |> Seq.filter (fun num -> not (num |> List.contains subcuadricula))//asigna posibles valores a la cuadricula
+                |> Seq.toList//regresa la union de las secuencias anteriores
+            
+        [ for (i,j) in indicesVacios do//creara un tablero nuevo con los posibles valores apartir de donde se encontro el 0
+            [ for num in movimientosValidos (i,j) -> 
+                let nuevoTablero = 
+                    estado
+                    |> List.mapi (fun r fila -> 
+                        fila 
+                        |> List.mapi (fun c valor -> 
+                            if r=i && c=j then num else valor))
+                nuevoTablero ] ]
 
     let problema estado = {
         inicio = estado
@@ -112,8 +79,3 @@ module sudoku =
         meta = meta
         costo = costo
     }
-
-    let h1 nodo = 
-        List.zip goal nodo.estado
-        |> List.sumBy (fun (x,y) -> if x <> y && x <> 0 then 1.0 else 0.0)
-*)
